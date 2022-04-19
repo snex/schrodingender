8489627595d692a4cdaca8f27ec58450a832bdea
David Havlicek <snex00@gmail.com>
2022-04-19 09:54:37 -0700

    do some code cleanup

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -8,25 +8,11 @@ class Signature < ApplicationRecord
   before_validation :set_api_key
 
   def generate_signature_image
-    pronouns = "#{NonsenseWordGenerator.generate_nonsense_word}/#{NonsenseWordGenerator.generate_nonsense_word}/#{NonsenseWordGenerator.generate_nonsense_word}"
-    draw = Magick::Draw.new
-    draw.pointsize   = 20
-    draw.fill        = '#000000'
-    draw.gravity     = Magick::NorthWestGravity
-    draw.font_weight = 100
-    draw.font_family = 'Arial'
-    draw.font_style  = Magick::NormalStyle
-
+    pronouns = generate_signature_pronouns
+    draw = set_draw_context
     static_signature_metrics = draw.get_multiline_type_metrics(self.static_signature)
     pronouns_metrics = draw.get_type_metrics(pronouns)
-
-    image = Magick::Image.new(
-      [static_signature_metrics.width, pronouns_metrics.width].max + 20,
-      static_signature_metrics.height + pronouns_metrics.height + 20,
-      Magick::SolidFill.new(self.background_color)
-    )
-    image.format = 'png'
-
+    image = generate_new_image(static_signature_metrics, pronouns_metrics)
     draw.annotate(image, static_signature_metrics.width, static_signature_metrics.height, 10, 10, self.static_signature)
     RainbowAnnotator.draw_rainbow(pronouns, draw, image, 10 + static_signature_metrics.height)
 
@@ -35,8 +21,37 @@ class Signature < ApplicationRecord
 
   private
 
+  def generate_new_image(static_signature_metrics, pronouns_metrics)
+    image = Magick::Image.new(
+      [static_signature_metrics.width, pronouns_metrics.width].max + 20,
+      static_signature_metrics.height + pronouns_metrics.height + 20,
+      Magick::SolidFill.new(self.background_color)
+    )
+    image.format = 'png'
+
+    return image
+  end
+
+  def generate_signature_pronouns
+    Array.new(3) do
+      NonsenseWordGenerator.generate_nonsense_word
+    end.join('/')
+  end
+
   def set_api_key
     return if self.api_key.present?
     self.api_key = SecureRandom.uuid
+  end
+
+  def set_draw_context
+    draw = Magick::Draw.new
+    draw.pointsize   = 20
+    draw.fill        = '#000000'
+    draw.gravity     = Magick::NorthWestGravity
+    draw.font_weight = 100
+    draw.font_family = 'Arial'
+    draw.font_style  = Magick::NormalStyle
+
+    return draw
   end
 end
